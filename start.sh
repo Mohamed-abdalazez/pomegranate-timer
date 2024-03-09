546b8b26b91415719476b17d3489c4455f87333b
Mohamed-abdalazez <mohamed.abdalazez818@gmail.com>
2024-03-09 12:43:08 +0200

    Initialize the setup of start.sh.

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Initialize the year directory and create the current date.json file.
+initialize(){}
+
+# Welcome Message.
+wlecome_message(){}
+
+# Start the new focus session.
+start_session(){}
+
+# Play alarm after the timer is done.
+play_alarm(){}
+
+# Save the session information.
+save_session_info(){}
+
+# Display the fun art for simple decoration.
+display_pomegranate_timer
+
+# Main function.
+main()
