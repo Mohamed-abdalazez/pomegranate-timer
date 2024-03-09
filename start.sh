76f266ab65dad346b896242659b7b37bb7be6289
Mohamed-abdalazez <mohamed.abdalazez818@gmail.com>
2024-03-09 13:10:18 +0200

    Added: Implementation of initialize(){}.

--- start.sh
+++ start.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
 # Initialize the year directory and create the current date.json file.
-initialize(){}
+initialize() {
+  year=$(date +"%Y")
+  month=$(date +"%m")
+  day=$(date +"%d")
+
+  year_dir="$year"
+  today_json="$year_dir/$month-$day-$year.json"
+
+  # Create directory if it doesn't exist.
+  mkdir -p "$year_dir"
+
+  # Create JSON file if it doesn't exist.
+
+  if [ ! -f "$today_json" ]; then
+    touch "$today_json"
+  fi
+}
 
 # Welcome Message.
-wlecome_message(){}
+#wlecome_message() {}
 
 # Start the new focus session.
-start_session(){}
+#start_session() {}
 
 # Play alarm after the timer is done.
-play_alarm(){}
+#play_alarm() {}
 
 # Save the session information.
-save_session_info(){}
+#save_session_info() {}
 
 # Display the fun art for simple decoration.
-display_pomegranate_timer
+#display_pomegranate_timer
 
 # Main function.
-main()
+main() {
+  initialize
+}
+# Execute main function
+main
